ac07da45e83b5eb84d3d1d1d5c14f985886986b4
dimdbr <ddd260501@gmail.com>
2021-05-16 13:33:35 +0300

    global change to CucumberRunner + all automatic + scroll were needed + correct waiting

--- src/test/java/webpages/ContactUsFormPage.java
+++ src/test/java/webpages/ContactUsFormPage.java
@@ -6,67 +6,80 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class ContactUsFormPage {
     private WebDriver driver;
-    @FindBy(name ="user_first_name" )
-    WebElement userName;
-    @FindBy(name ="user_last_name" )
-    WebElement userLastName;
-    @FindBy(name ="user_email" )
-    WebElement userEmail;
-    @FindBy(name ="user_phone" )
-    WebElement userPhone;
-    @FindBy(className="checkbox__label")
-    List<WebElement> checkBoxes;
-    @FindBy(className = "button-ui")
-    List<WebElement> buttons;
+    By userNameL = By.name("user_first_name");
+    By userLastNameL = By.name("user_last_name");
+    By userEmailL = By.name("user_email");
+    By userPhoneL = By.name("user_phone");
+    By checkBoxes = By.className("checkbox__label");
+    By buttons = By.className("button-ui");
 
 
     public void setUserName(String userName) {
-        this.userName.sendKeys(userName);
+        driver.findElement(userNameL).sendKeys(userName);
     }
 
     public void setUserLastName(String userLastName) {
-        this.userLastName.sendKeys(userLastName);
+        driver.findElement(userLastNameL).sendKeys(userLastName);
     }
 
     public void setUserEmail(String userEmail) {
-        this.userEmail.sendKeys(userEmail);
+        driver.findElement(userEmailL).sendKeys(userEmail);
     }
+
     public void setUserPhone(String userPhone) {
-        this.userPhone.sendKeys(userPhone);
+
+        driver.findElement(userPhoneL).sendKeys(userPhone);
     }
 
 
-    public void fillFields(String firstName,String lastName,String email,String phone)
-    {
+    public void fillFields(String firstName, String lastName, String email, String phone) {
         this.setUserName(firstName);
         this.setUserLastName(lastName);
         this.setUserPhone(phone);
         this.setUserEmail(email);
     }
-    public void clickCheckbox()
-    {
-        WebElement cb = checkBoxes.get(0);
+
+    public void clickCheckbox() {
+        WebElement cb = driver.findElements(checkBoxes).get(0);
         cb.click();
     }
-    public String clickSubmit()
+    public boolean isSubmitClickable()
     {
-        WebElement submitBtn = buttons.get(1);
+        return isClickable(driver.findElements(buttons).get(1));
+    }
+    public void clickSubmit() {
+        WebElement submitBtn = driver.findElements(buttons).get(1);
         Actions actionProvider = new Actions(driver);
         actionProvider.moveToElement(submitBtn).build().perform();
         submitBtn.click();
-        return userName.getCssValue("color");
+//        return driver.findElement(userNameL).getCssValue("color");
     }
 
     public ContactUsFormPage(WebDriver driver) {
         this.driver = driver;
-        PageFactory.initElements(driver,this);
+        this.driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+
+//        wait for loading page
+        this.driver.manage().timeouts().pageLoadTimeout(10000, TimeUnit.MILLISECONDS);
     }
 
+    public boolean isClickable(WebElement webe) {
+        try {
+            WebDriverWait wait = new WebDriverWait(this.driver, 4);
+            wait.until(ExpectedConditions.elementToBeClickable(webe));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 
 }
--- src/test/java/webpages/EpamMainPage.java
+++ src/test/java/webpages/EpamMainPage.java
@@ -7,81 +7,88 @@ import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class EpamMainPage {
     private WebDriver driver;
-    @FindBy(xpath = "//*[text()='Services']")
-    WebElement redirectLink;
-    @FindBy(xpath = "//*[text()='Consult']")
-    WebElement consultBtn;
-    @FindBy(className = "rollover-tiles__description")
-    List<WebElement> rolloverTile;
-    @FindBy(className = "slider__dot")
-    List<WebElement> listOfSliderDots;
-    @FindBy(className = "button-ui")
-    List<WebElement> slider;
-    @FindBy(className = "location-selector__button")
-    WebElement locationSelectorBtn;
-    @FindBy(className = "location-selector__panel")
-    WebElement selectorPanel;
-    @FindBy(className = "location-selector__link")
-    List<WebElement> ukranianLanguageBtn;
-    @FindBy(className = "header-search__button")
-    WebElement searchBtn;
-    @FindBy(id = "new_form_search")
-    WebElement searchBar;
-    @FindBy(className ="header-search__submit" )
-    WebElement findBtn;
+    By redirectLink = By.xpath("//*[text()='Services']");
+    By consultBtn = By.xpath("//*[text()='Consult']");
+    By rolloverTile = By.className("rollover-tiles__description");
+    By listOfSliderDot = By.className("slider__dot");
+    By slider = By.className("button-ui");
+    By locationSelectorBtn = By.className("location-selector__button");
+    By selectorPanel = By.className("location-selector__panel");
+    By ukranianLanguageBtn = By.className("location-selector__link");
+    By searchBtn = By.className("header-search__button");
+    By searchBar = By.id("new_form_search");
+    By findBtn = By.className("header-search__submit");
 
 
     public void clickServices() {
-        redirectLink.click();
+        driver.findElement(redirectLink).click();
     }
 
     public void mouseOverConsultBtn() {
         Actions action = new Actions(driver);
-        action.moveToElement(consultBtn);
-        action.moveToElement(rolloverTile.get(0)).build().perform();
+        action.moveToElement(driver.findElement(consultBtn));
+
+        action.moveToElement(driver.findElements(rolloverTile).get(0)).build().perform();
 
     }
 
     public boolean isVisible() {
-        return rolloverTile.get(0).isDisplayed();
+        return driver.findElements(rolloverTile).get(0).isDisplayed();
     }
 
-    public boolean clickSlider() {
+    public void clickSlider() {
         //System.out.println(listOfSliderDots.size());
         //System.out.println(slider.size());
         //System.out.println(isClickable(slider.get(6)));
-        listOfSliderDots.get(3).click();
-        return isClickable(slider.get(6));
+        driver.findElements(listOfSliderDot).get(3).click();
     }
 
-    public boolean popUpAfterClick() {
-        locationSelectorBtn.click();
-        System.out.println(selectorPanel.isDisplayed());
-        return selectorPanel.isDisplayed();
+    public boolean isSliderClickable() {
+        return isClickable(driver.findElements(slider).get(6));
+    }
+
+    public void clickLanguageSelector() {
+        driver.findElement(locationSelectorBtn).click();
+
+
+    }
+
+    public boolean isPanelVisible() {
+        return driver.findElement(selectorPanel).isDisplayed();
     }
 
     public void changeLanguage() {
-        locationSelectorBtn.click();
+        driver.findElement(locationSelectorBtn).click();
         Actions action = new Actions(driver);
-        action.moveToElement(ukranianLanguageBtn.get(7)).build().perform();
-        ukranianLanguageBtn.get(7).click();
+        action.moveToElement(driver.findElements(ukranianLanguageBtn).get(7)).build().perform();
+        driver.findElements(ukranianLanguageBtn).get(7).click();
+
     }
 
     public EpamMainPage(WebDriver driver) {
+
         this.driver = driver;
-        PageFactory.initElements(driver, this);
+        this.driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+
+//        wait for loading page
+        this.driver.manage().timeouts().pageLoadTimeout(10000, TimeUnit.MILLISECONDS);
+
+//         wait for an asynchronous script to finish execution
+//        this.driver.manage().timeouts().setScriptTimeout(5000, TimeUnit.MILLISECONDS);
     }
 
     public boolean isClickable(WebElement webe) {
         try {
-            WebDriverWait wait = new WebDriverWait(this.driver, 5);
+            WebDriverWait wait = new WebDriverWait(this.driver, 4);
             wait.until(ExpectedConditions.elementToBeClickable(webe));
             return true;
         } catch (Exception e) {
@@ -90,15 +97,14 @@ public class EpamMainPage {
     }
 
     public void clickSearchBtn() {
-        searchBtn.click();
+        driver.findElement(searchBtn).click();
     }
 
-    public void setSearchBar(String request)
-    {
-        this.searchBar.sendKeys(request);
+    public void setSearchBar(String request) {
+        driver.findElement(searchBar).sendKeys(request);
     }
-    public void clickToFind()
-    {
-        findBtn.click();
+
+    public void clickToFind() {
+        driver.findElement(findBtn).click();
     }
 }
